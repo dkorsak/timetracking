f66525b3981138648e286e4abed1e31b839ef9b7
Daniel Korsak <daniel.korsak@redexperts.net>
2013-01-13 19:09:11 +0100

    Model

--- cms/web/app.php
+++ cms/web/app.php
@@ -10,7 +10,7 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance
 // Change 'sf2' by the prefix you want in order to prevent key conflict with another application
 
-$loader = new ApcClassLoader('symfony-sandbox', $loader);
+$loader = new ApcClassLoader('timetracking', $loader);
 $loader->register(true);
 
 
