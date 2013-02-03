dd4e7a2d2a2d5ab009f75162af67466873c8282f
Daniel Korsak <daniel.korsak@redexperts.net>
2013-02-03 15:16:07 +0100

    Datetime libraries

--- cms/src/App/FrontendBundle/Controller/TimesheetWeekController.php
+++ cms/src/App/FrontendBundle/Controller/TimesheetWeekController.php
@@ -6,8 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TimesheetWeekController extends Controller
 {
-    public function indexAction()
+    public function indexAction($year = "", $week = "")
     {
-        return $this->render('AppFrontendBundle:TimesheetWeek:index.html.twig');
+        if ($year == "" || $week = "") {
+            
+        }
+        $currentDate = new \DateTime("now");
+        return $this->render(
+            'AppFrontendBundle:TimesheetWeek:index.html.twig',
+            array(
+                'currentDate' => $currentDate
+            )
+        );
     }
 }
